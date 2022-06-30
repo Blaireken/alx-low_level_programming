39aafd1a086283f0a7f9be3653ec4fb55602bc9d
Kennedy Muriithi <muriithiken66@gmail.com>
2022-06-30 11:15:10 -0700

    C-programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,14 @@
 /**
  * main: Entry point
  *
+ * Descriptiom: puts output
+ *
  * return: Always 0
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return 0;
 }
