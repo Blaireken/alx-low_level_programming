c9183576679e799e490abe05134ac22444ab98dd
Kennedy Muriithi <muriithiken66@gmail.com>
2022-06-30 10:36:42 -0700

    C-programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
  * printf - prints "with proper grammar, but the outcome is a piece of art,"
  * Description: prints with proper grammar, but the outcome is a piece of art,
  */
-int printf(void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
