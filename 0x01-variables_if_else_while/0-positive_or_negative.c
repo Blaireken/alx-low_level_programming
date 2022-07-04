6def5c0a99984eeb5e6a04f4f04488440cc98f52
Kennedy Muriithi <muriithiken66@gmail.com>
2022-07-04 01:07:59 -0700

    variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,8 +8,8 @@
 /**
  * main - Entry point
  *
- * Description: This program will assign a random number to 
- * the variable n each time it is executed. Complete the 
+ * Description: This program will assign a random number to
+ * the variable n each time it is executed. Complete the
  * source code in order to print whether the number stored
  *  in the variable n is positive or negative.
  *  Return: Always 0 (sucess)
