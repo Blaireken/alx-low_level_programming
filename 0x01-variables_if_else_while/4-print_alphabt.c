f1aedbe65abfc2a7335575d003e03e8a1e5be1fe
Kennedy Muriithi <muriithiken66@gmail.com>
2022-07-04 02:01:39 -0700

    variables

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,11 +6,12 @@
  * Description: Write a program that prints the alphabet
  * in lowercase, followed by a new line.Print all the letters
  * except q and e.use putchar twice in your code
- * return: Always 0 .
+ * Return: Always 0 (success)
  */
 int main(void)
 {
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		if (letter != 'e' && letter != 'q')
