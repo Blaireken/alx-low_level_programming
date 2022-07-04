8629b5f16d7e4d643f4e9bcdd5bbaedfd9ca8687
Kennedy Muriithi <muriithiken66@gmail.com>
2022-07-04 00:51:26 -0700

    advance tasks

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 /**
  * main:Entry point 
@@ -9,8 +10,10 @@
  */
 int main(void)
 {
-	system("echo 'and  that piece of art is useful
-			\" - Dora Korpar, 2015-10-19.\n'");
+	char *s = "and  that piece of art is useful
+		\" - Dora Korpar, 2015-10-19.\n'");
+
+	write(1, s, strlen(s));
 
 	return (1);
 
