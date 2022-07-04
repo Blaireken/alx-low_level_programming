77a78f6e34d924c1deb37f35b1598602aaa4c01a
Kennedy Muriithi <muriithiken66@gmail.com>
2022-07-04 00:19:25 -0700

    advance tasks

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,10 +6,10 @@
  *
  *return : Always 1 (success)
  */
-int main (void)
+int main(void)
 {
-	write("and  that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\r.",59);
+	system("echo 'and  that piece of art is useful
+			\" - Dora Korpar, 2015-10-19.\n'");
 
 	return (1);
 
