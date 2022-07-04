a8879b585abbae6c68475c60e7389207d6302b48
Kennedy Muriithi <muriithiken66@gmail.com>
2022-07-04 00:07:27 -0700

    advance tasks

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include <stlib.h>
 
 /**
- * main: UNIX is basically a simple operating system.
+ * main:Entry point 
  *
  *return : Always 1 (success)
  */
 int main (void)
 {
-	write(	STDOUT_FILRNO, "and  that piece of art is useful
+	write(STDOUT_FILRNO, "and  that piece of art is useful
 			\" - Dora Korpar, 2015-10-19\r.",59);
 
 	return (1);
