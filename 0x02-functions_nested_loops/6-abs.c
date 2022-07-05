cc2921aa950dde82b21c81be94b6538cbeb4df52
Kennedy Muriithi <muriithiken66@gmail.com>
2022-07-05 09:14:12 -0700

    nested loops

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,8 +2,9 @@
 
 /**
  * _abs - function that computes the absolute value of an integer.
- * @a: integer to be checked. 
- * Return: The absolute value of integer.
+ * @a: integer to be checked
+ *
+ * Return: The absolute value of integer
  */
 int _abs(int a)
 {
