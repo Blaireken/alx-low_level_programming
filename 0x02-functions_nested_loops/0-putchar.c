0edcb41a4bed9c50eb436695beb19814da12085d
Kennedy Muriithi <muriithiken66@gmail.com>
2022-07-05 05:52:26 -0700

    nested loop

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints _putchar
+ * followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("_putchar");
+
+	return (0);
+
+}
