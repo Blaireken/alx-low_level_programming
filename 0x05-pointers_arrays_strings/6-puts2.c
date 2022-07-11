ae33c124f96b1da9901ea134d4075d7ee86684b3
Kennedy Muriithi <muriithiken66@gmail.com>
2022-07-11 14:22:41 -0700

    arrays&pointers

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,7 +8,7 @@
 void puts2(char *str)
 {
 	int i;
-	
+
 	i = 0;
 	for (; str[i] != '\0'; i++)
 	{
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,7 +6,8 @@
  *  pointed to by dest
  *  @dest: buffer pointer
  *  @src: ponter
- *  Return: string
+ *
+ * Return: string dest
  */
 char *_strcpy(char *dest, char *src)
 {
