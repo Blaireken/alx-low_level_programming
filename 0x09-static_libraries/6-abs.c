f28a568d6356872b4928a6d1f393155bd838a784
Kennedy Muriithi <muriithiken66@gmail.com>
2022-07-24 03:44:26 -0700

    static lib

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - function that computes the absolute value of an integer.
+ * @a: integer to be checked
+ *
+ * Return: The absolute value of integer
+ */
+int _abs(int a)
+{
+	if (a >= 0)
+		return (a);
+
+	else
+		return (-a);
+}
+
---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * _strcpy - function that copies the string pointed to by src,
+ * including the terminating null byte (\0), to the buffer
+ *  pointed to by dest
+ *  @dest: buffer pointer
+ *  @src: ponter
+ *
+ * Return: string dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	for (; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
