626b13c086e6a2bd7bb26ca6e4b7d51b258228d8
Kennedy Muriithi <muriithiken66@gmail.com>
2022-08-11 06:32:53 -0700

    singly linked list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,23 +1,21 @@
 #include "lists.h"
 
 /**
- * list_len - a function that returns the number
- *  of elements in a linked list_t list
- * @h: input
- * Return: 0
+ * list_len - Finds the number of elements in
+ *            a linked list_t list.
+ * @h: The linked list_t list.
+ *
+ * Return: The number of elements in h.
  */
-
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
-	int count = 0;
+	size_t elements = 0;
 
-	if (h == NULL)
-		return (0);
-
-	while (h != NULL)
+	while (h)
 	{
-		count++;
+		elements++;
 		h = h->next;
 	}
-	return (count);
+
+	return (elements);
 }
